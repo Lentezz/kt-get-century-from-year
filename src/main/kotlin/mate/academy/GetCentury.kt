e6c4ec709a8178ded479c2200157eb2a49aeee8b
Lentezz <yaroslav.golovan@gmail.com>
2025-08-31 15:31:10 +0300

    Fixed

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,7 +1,8 @@
 package mate.academy
 
 const val CENTURY_YEARS = 100
+const val CENTURY_BOUNDARY_INCREMENT = 1
 
 fun getCentury(year: Int) : Int {
-    return if (year % 100 == 0) year / CENTURY_YEARS else year / CENTURY_YEARS + 1
+    return if (year % 100 == 0) year / CENTURY_YEARS else year / CENTURY_YEARS + CENTURY_BOUNDARY_INCREMENT
 }
